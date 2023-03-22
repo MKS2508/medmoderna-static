e894595a27909deddee44474decf60382e7f62e6
Marcos Asensio <dam02.2020.jesuitas@gmail.com>
2023-03-22 10:58:17 +0100

    a

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 const express = require('express')
 const helmet = require('helmet')
-const path = require('path')
 
 const app = express()
 
 // add some security-related headers to the response
 app.use(helmet())
 
-// Configura el middleware para servir archivos estáticos
-app.use('/videos', express.static(path.join(__dirname, 'public', 'videos')))
-
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.set('Content-Type', 'text/html')
-    res.status(200).send(`
-        <style>video{width:100%;max-width:500px;}</style>
+    res.status(200).send(`<style>video{width:100%;max-width:500px;}</style>
         <div class="videos">
             <video src="/videos/videohome3.mp4" controls></video>
-        </div>
-    `)
+        </div>`)
 })
 
 module.exports = app
